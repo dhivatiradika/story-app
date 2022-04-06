ccd3c8d3736917c1a898411abaa3c0e92eee14a8
dhivatiradika <dhivatiradika@gmail.com>
2022-04-07 00:31:34 +0800

    add motion layout to login activity

---
+++ app/src/main/java/com/dhiva/storyapp/ui/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.dhiva.storyapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dhiva.storyapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
